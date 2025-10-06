79be66a72a90469d468ef85d0add7b20696d649f
WenHao2130 <wenhao2130@outlook.com>
2025-10-06 18:22:17 +0800

    sdm660-common: Build android.hardware.drm@1.3
    Change-Id: Icc2e02d7b1187d99007c7fb3a520dc69137025db
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- common.mk
+++ common.mk
@@ -123,10 +123,10 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.2.vendor
+    android.hardware.drm@1.3.vendor
 
 # GNSS
 PRODUCT_PACKAGES += \
