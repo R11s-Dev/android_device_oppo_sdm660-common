46b9b1ad47595cceb5d3c4dbaa4cfffb4ef5c5b8
HuaLiMao-AQ <hualimao_aq@163.com>
2025-11-06 17:27:29 +0800

    Revert "sdm660-common: Build android.hardware.bluetooth@1.0-1"
    This reverts commit c08267ad86bf8b07a70efe508deafa9263bf3f03.

--- common.mk
+++ common.mk
@@ -69,9 +69,7 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor \
-    android.hardware.bluetooth@1.0.vendor \
-    android.hardware.bluetooth@1.1.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml
