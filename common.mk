d293001afbe0bfdb13a81915b671e482eb9edd5b
HuaLiMao-AQ <hualimao_aq@163.com>
2025-11-06 22:33:34 +0800

    sdm660-common: Add vendor.qti.hardware.btconfigstore@1.1::IBluetoothSar/default and build libbluetooth
    bug tracking:
    I hwservicemanager: getTransport: Cannot find entry vendor.qti.hardware.btconfigstore@1.0::IBTConfigStore/default in either framework or device manifest.
    
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- common.mk
+++ common.mk
@@ -69,7 +69,8 @@ PRODUCT_PACKAGES += \
     audio.bluetooth.default \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    libbluetooth
 
 PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/bluetooth_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/bluetooth_audio_policy_configuration.xml
