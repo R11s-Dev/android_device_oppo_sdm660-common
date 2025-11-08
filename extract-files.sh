83ece4ac30d8255beed5f6b45365c09a5a558f36
HuaLiMao-AQ <hualimao_aq@163.com>
2025-11-08 21:15:10 +0800

    sdm660-common: Open the binary file camera from /vendor/etc/camera
    bug tracking:
    E QCamera : <MCI><ERROR> file_copy: 2829: open source file /system/etc/camera/cp/FusionCalibrationParams.bin failed
    E QCamera : <MCI><ERROR> file_copy: 2829: open source file /system/etc/camera/cp/SystemParamsBokeh.bin failed
    E QCamera : <MCI><ERROR> file_copy: 2829: open source file /system/etc/camera/cp/SystemParamsFusion.bin failed
    E QCamera : <MCI><ERROR> file_copy: 2829: open source file /system/etc/camera/cp/CPCalibrationData.bin failed
    
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- extract-files.sh
+++ extract-files.sh
@@ -69,6 +69,9 @@ function blob_fixup() {
         vendor/lib64/libril-qc-hal-qmi.so)
         "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v28.so" "${2}"
         ;;
+        vendor/lib/libmmcamera_interface.so)
+        sed -i 's|/system/etc/camera|/vendor/etc/camera|g' "${2}"
+        ;;
     esac
 }
 
