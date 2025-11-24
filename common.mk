5bef3b4544566d451381b07cf4376b15dbca437b
HuaLiMao-AQ <hualimao_aq@163.com>
2025-11-24 13:02:31 +0800

    Revert "sdm660-common: sdm660-common: Build missing Bluetooth A2DP"
    This reverts commit 650fb3afbe5acc6a980e9f2e6a0ef6a688e4219f.
    
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>

--- common.mk
+++ common.mk
@@ -75,13 +75,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_product_strategies.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_product_strategies.xml \
     frameworks/av/services/audiopolicy/enginedefault/config/example/phone/audio_policy_engine_stream_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_engine_stream_volumes.xml
 
-# A2DP
-PRODUCT_PACKAGES += \
-    liba2dpoffload \
-    android.hardware.bluetooth.a2dp@1.0.vendor \
-    android.hardware.bluetooth.a2dp@1.0-impl \
-    android.hardware.bluetooth.a2dp@1.0-service
- 
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
