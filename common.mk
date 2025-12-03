ef0f330a5b0564e5b112ffe056920f49972a8ecb
Erfan Abdi <erfangplus@gmail.com>
2025-12-03 21:43:05 +0800

    sdm660-common: fingerprint: Write Fingerprint HAL Adapter
    vendor.oppo.hardware.biometrics.fingerprint@2.1 -> android.hardware.biometrics.fingerprint@2.1
    
    Change-Id: Id33db73425bb5e92ef5c45e2f8d40fa462942af3
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- common.mk
+++ common.mk
@@ -156,6 +156,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.oppo_sdm660
+
 # GNSS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
