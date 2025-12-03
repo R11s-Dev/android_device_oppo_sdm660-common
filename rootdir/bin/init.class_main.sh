39f9f737ae622ce53b0988de46b17b722e98cdf2
HuaLiMao-AQ <hualimao_aq@163.com>
2025-12-04 00:28:29 +0800

    sdm660-common: Remove qmuxd service
    * Not required
    
    Change-Id: I3ef83f48d4577ac74b042147f0dcbc5bb2db2eb0
    Signed-off-by: HuaLiMao-AQ <hualimao_aq@163.com>
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- rootdir/bin/init.class_main.sh
+++ rootdir/bin/init.class_main.sh
@@ -50,11 +50,6 @@ case "$baseband" in
 esac
 
 case "$baseband" in
-    "msm" | "csfb" | "svlte2a" | "mdm" | "mdm2" | "sglte" | "sglte2" | "dsda2" | "unknown" | "dsda3")
-    start vendor.qmuxd
-esac
-
-case "$baseband" in
     "msm" | "csfb" | "svlte2a" | "mdm" | "mdm2" | "sglte" | "sglte2" | "dsda2" | "unknown" | "dsda3" | "sdm" | "sdx" | "sm6")
 
     # For older modem packages launch ril-daemon.
