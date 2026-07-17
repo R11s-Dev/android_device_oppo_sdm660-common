119cf5b66be89d8201c0fd54cf89de72e73e09f7
Mohammad Hasan Keramat J <ikeramat@protonmail.com>
2026-07-17 20:59:50 +0800

    sdm660-common: Update display blobs from S62Pro
    Change-Id: I39b4e5b38e93dbf09100a333b08dd40e753b7a85
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- common.mk
+++ common.mk
@@ -106,9 +106,9 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.mapper@2.0-impl-2.1 \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.mapper@3.0-impl-qti-display \
+    android.hardware.graphics.mapper@4.0-impl-qti-display \
+    vendor.qti.hardware.display.allocator-service \
     gralloc.sdm660
 
 PRODUCT_PACKAGES += \
@@ -121,25 +121,17 @@ PRODUCT_PACKAGES += \
     memtrack.sdm660
 
 PRODUCT_PACKAGES += \
-    libdisplayconfig.qti \
+    android.frameworks.displayservice@1.0_32 \
+    android.frameworks.displayservice@1.0.vendor \
+    android.frameworks.displayservice@1.0 \
+    libdisplayconfig \
+    libgralloc.qti \
     libqdMetaData \
+    libqdMetaData.system \
     libtinyxml \
-    vendor.display.config@1.0.vendor \
     vendor.display.config@2.0 \
-    vendor.qti.hardware.display.mapper@2.0.vendor \
-    vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.display.config@1.0.vendor \
-    vendor.display.config@1.1.vendor \
-    vendor.display.config@1.2.vendor \
-    vendor.display.config@1.3.vendor \
-    vendor.display.config@1.4.vendor \
-    vendor.display.config@1.5.vendor \
-    vendor.display.config@1.6.vendor \
-    vendor.display.config@1.7.vendor \
-    vendor.display.config@1.8.vendor \
-    vendor.display.config@1.9.vendor \
-    vendor.display.config@1.10.vendor \
-    vendor.display.config@2.0.vendor
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@2.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
