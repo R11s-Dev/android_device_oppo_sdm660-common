2df42d7b81c006a0fdda120b8aa2c8b4732f5349
LuK1337 <priv.luk@gmail.com>
2026-07-17 20:50:45 +0800

    sdm660-common: Include device FCM from vendor/lineage
    Change-Id: Ia29d6fb047a09a2b4767312cb2a3252c87095723
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -150,6 +150,8 @@ PRODUCT_FULL_TREBLE_OVERRIDE := true
 TARGET_COPY_OUT_VENDOR := vendor
 
 # VINTF
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    vendor/lineage/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
 DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 
