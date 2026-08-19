9fa04f2060eb1ab0bb2fe48d87dba3d345d6614e
Cosmin Tanislav <demonsingur@gmail.com>
2026-08-19 20:45:08 +0800

    sdm660-common: Build libstagefright_softomx_plugin
    vndksupport: Could not load libstagefright_softomx_plugin.so from default namespace: dlopen failed: library "libstagefright_softomx_plugin.so" not found
    
    Change-Id: I56b620ebb9f9dc64ecc492bdb7ee17c3df57e593
    Signed-off-by: ltdq <cyzi7777@163.com>

--- common.mk
+++ common.mk
@@ -226,7 +226,8 @@ PRODUCT_PACKAGES += \
     libOmxQcelp13Enc \
     libOmxVdec \
     libOmxVenc \
-    libstagefrighthw
+    libstagefrighthw \
+    libstagefright_softomx_plugin.vendor
 
 # Overlays
 PRODUCT_PACKAGES += \
