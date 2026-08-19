02e34ff83a03c539a911616950ffd64fd57c2cd7
me-cafebabe <me_cafebabe@hotmail.com>
2026-08-19 14:10:21 +0800

    sdm660-common: Switch to USB 1.3-basic HAL
    Change-Id: I6f477db2220b3563b44bb9545b7889166b8a6b06
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- common.mk
+++ common.mk
@@ -377,7 +377,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.3-service.basic
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
