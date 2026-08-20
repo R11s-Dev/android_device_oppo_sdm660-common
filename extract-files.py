49530d6bb1cb5b922f330da69bb9995065a0f3d3
Michael Bestas <mkbestas@lineageos.org>
2026-08-20 21:54:18 +0800

    sdm660-common: Add missing IMS libs
    Change-Id: Ie7ddf49bed75c823a27dcc4414c0d5ab307410fd
    Signed-off-by: ltdq <cyzi7777@163.com>

--- extract-files.py
+++ extract-files.py
@@ -38,6 +38,7 @@ lib_fixups: lib_fixups_user_type = {
     (
         'com.qualcomm.qti.dpm.api@1.0',
         'vendor.qti.hardware.fm@1.0',
+        'vendor.qti.imsrtpservice@3.0.so',
     ): lib_fixup_vendor_suffix,
 }
 
