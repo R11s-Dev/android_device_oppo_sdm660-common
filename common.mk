0c733f3b890525686899e60a901190bb07bbfcc5
Michael Bestas <mkbestas@lineageos.org>
2026-08-20 15:08:10 +0800

    sdm660-common: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Idb002fc81fca3321f70fb3bba83f4af44354ce81
    Signed-off-by: ltdq <cyzi7777@163.com>

--- common.mk
+++ common.mk
@@ -109,7 +109,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
-    hwcomposer.sdm660
+    hwcomposer.qcom
 
 PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
